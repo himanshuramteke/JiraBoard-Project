4eaf023832ec767fc3fe70adc369a348369a8641
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-02-07 11:34:09 +0530

    Added chakra ui and dnd-kit core library

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
+import { Provider } from './components/ui/provider.tsx'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <Provider>
     <App />
-  </StrictMode>,
+  </Provider>,
 )
